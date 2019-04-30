505d196aa487182b8794b52b0a51d7ceb2717846
Mary Selig <seligmar@gmail.com>
2019-04-30 19:18:57 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,18 +1,9 @@
 # Your code goes here!
 
 class Dog 
-  # def initialize(name, bark)
-  #   @name = name
-  #   @bark = bark 
-  # end
-  
   def name=(name)
     @name = name
   end 
-  
-  # # def bark=(bark)
-  # #   @bark = bark 
-  # # end 
 
   def name
     @name
